05c5c149fb4d43525c94ef4239f4b7bc74236245
Damanpreet Kaur <damanpreetkaur@10-197-140-82.sds.oregonstate.edu>
2020-09-15 16:42:08 -0700

    Updated scripts

--- read_labels.py
+++ read_labels.py
@@ -7,6 +7,66 @@ import os
 import argparse
 from utils import *
 from config import config as cfg
+import re
+
+# Process the XML file with incorrect hierarchy.
+def process_incorrect_xml(label_path, fpath, file):
+    # extract a single channel from the label.
+    label_image = cv2.imread(label_path)[:,:,0] 
+
+    # create two dictionaries - one for the explants and other for the tissue parts.
+    explants_map = dict()
+    tissues_map = dict()
+
+    saveFlag = True
+
+    # Parse the file into a tree.
+    file = os.path.join(fpath, file)
+    tree = ET.parse(file)
+    # fetch the root node.
+    root = tree.getroot()
+
+    for c in root:
+        if c.tag == "hierarchyStack":
+            for i, child in enumerate(c, 1):
+                tissue_explant = child.find('name').text
+                explant_id = int(re.sub("[a-zA-Z]", '', tissue_explant))
+
+                if explant_id not in explants_map:
+                    explants_map[explant_id] = []
+                
+                classes = child.find('classes')    
+                if classes:
+                    for j, child2 in enumerate(classes):
+
+                        # obtain the plant tissue name and its corresponding id.
+                        tname = child2.find('name').text.lower()
+                        uid = int(child2.find('uid').text)
+
+                        if tname not in tissues_map:
+                            tissues_map[tname] = []
+                        
+                        # update the dictionaries
+                        tissues_map[tname].append(uid)
+                        explants_map[explant_id].append(uid)
+
+                    # create a plant tissues image.
+                    tissues_image = np.zeros(label_image.shape)
+                    for tname in tissues_map:
+                        for uid in tissues_map[tname]:
+                            tissues_image[label_image==uid] = cfg.assign_labels[tname]
+                    
+                    # explant image
+                    explant_image = np.zeros(label_image.shape)
+                    for explant in explants_map:
+                        for uid in explants_map[explant]:
+                            explant_image[label_image==uid] = explant
+                    
+                else:
+                    saveFlag = False
+                    print("Hierarchy stack is empty.")
+                    return saveFlag, _, _
+    return saveFlag, tissues_image, explant_image
 
 
 # Function to process the labels of an image
@@ -14,18 +74,17 @@ def process_label(label_path, fpath, file):
     # extract a single channel from the label.
     label_image = cv2.imread(label_path)[:,:,0] 
 
-    # Parse the file into a tree.
-    file = os.path.join(fpath, file)
-    tree = ET.parse(file)
-    
-    # fetch the root node.
-    root = tree.getroot()
-
     # create two dictionaries - one for the explants and other for the tissue parts.
     explants_map = dict()
     tissues_map = dict()
 
     saveFlag = True
+
+    # Parse the file into a tree.
+    file = os.path.join(fpath, file)
+    tree = ET.parse(file)
+    # fetch the root node.
+    root = tree.getroot()
 
     # fetch the hierarchy stack (Necrotic/Callus/Stem/Shoot)
     for c in root:
@@ -70,8 +129,9 @@ def process_label(label_path, fpath, file):
 # Function to parse the images.
 def parse_files(file_path, save_color):
     # create directories
-    deeplab_path = os.path.join(file_path, "deeplab")
-    ann_path = os.path.join(file_path, "annotation")
+    create_dir('labels')
+    deeplab_path = os.path.join('labels', "deeplab")
+    ann_path = os.path.join('labels', "annotation")
     create_dir(deeplab_path)
     create_dir(ann_path)
 
@@ -92,9 +152,6 @@ def parse_files(file_path, save_color):
     # list all the image files from the path.
     files = os.listdir(file_path)
 
-    # open file for writing
-    f = open(cfg.incorrect_xml, 'w')
-
     for file in files:
         if file.endswith('.xml'):
             # label image file
@@ -109,9 +166,11 @@ def parse_files(file_path, save_color):
 
             if saveFlag:
                 if len(np.unique(explant_image)) > 13:
-                    f.write(file+"\n")
-                    print("Incorrect XML file: ", file)
-                    continue
+                    saveFlag, tissues_image, explant_image = process_incorrect_xml(label_path, file_path, file)
+                    if not saveFlag:
+                        print("Incorrect XML file {} was not processed.".format(file))
+                        continue
+                    print("Incorrect XML file {} was processed by using alternate hierarchy configuration.".format(file))
 
                 # write label and tissues image.
                 tpath = os.path.join(tissues_deeplab_path, '.'.join(file.split('.')[:-1])+".png")
@@ -131,14 +190,12 @@ def parse_files(file_path, save_color):
                     save_color_image(tpath_ann, tissues_image, palette)
                     save_color_image(labpath_ann, explant_image, palette)
 
-    # close the file.
-    f.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Read labels')
     parser.add_argument("-fp", '--file_path', required=True, help='Path to the folders')
-    parser.add_argument("--save_color", action='store_false', help="Save colored labels.")
+    parser.add_argument("-save_color", action='store_false', help="Save colored labels.")
     args = parser.parse_args()
 
     # parse the files.
